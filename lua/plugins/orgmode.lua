429f0893267b24eaf62ef5c96f0cedf04dac7007
Will Daughtridge <wdaughtridge@gmail.com>
2025-04-09 13:07:23 -0400

    feat

--- lua/plugins/orgmode.lua
+++ lua/plugins/orgmode.lua
@@ -4,8 +4,8 @@ return {
   ft = { 'org' },
   config = function()
     require('orgmode').setup({
-      org_agenda_files = '~/orgfiles/**/*',
-      org_default_notes_file = '~/orgfiles/refile.org',
+      org_agenda_files = '~/org/**/*',
+      org_default_notes_file = '~/org/refile.org',
     })
   end,
 }
