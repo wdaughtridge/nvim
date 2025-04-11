4c3b517cb73f93d2a66460c55b0777fe88100f6a
Will Daughtridge <wdaughtridge@gmail.com>
2025-04-11 12:25:34 -0400

    feat

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,7 +6,7 @@ return {
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
-          ensure_installed = { "helm", "c", "lua", "vim", "vimdoc", "query", "c_sharp", "rust", "powershell", "javascript", "html" },
+          ensure_installed = { "go", "typescript", "helm", "c", "lua", "vim", "vimdoc", "query", "c_sharp", "rust", "powershell", "javascript", "html" },
           sync_install = false,
           highlight = { enable = true },
           indent = { enable = true },
